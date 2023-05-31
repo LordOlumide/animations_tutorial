b6bcc129afe6b9f244c260bdde4a09f5ffce7e6c
Olumide Olubosede <olumideolubosede@gmail.com>
2023-05-31 20:37:01 +0100

    Example 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animations_tutorial/app/screens/home_screen.dart';
+import 'package:animations_tutorial/app/screens/ex1_anim_builder_and_transform.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Animations tutorial',
-      home: HomeScreen(),
+      home: Example1(),
     );
   }
 }
